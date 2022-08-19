5a53c8ad899d35ba983bba479e91db1f8151e4fe
Tomasz <tomaszwoloszyn983@gmail.com>
2022-08-19 16:59:09 +0000

    Add sendEmail function to the contact.html page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -70,8 +70,8 @@ function fetchGitHubInformation(event) {
             } else if (errorResponse.status === 403) {
                 var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
                 $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
-                // This function prevents sending to many requests to the server
-                // 
+                // This function prevents sending to many requests to the server.
+                // Displays the expected time when the server is ready for new requests.
             }else{
                 console.log(errorResponse);
                 $("#gh-user-data").html(
---
+++ assets/js/sendEmail.js
@@ -0,0 +1,18 @@
+
+console.log("Hello from send Email!");
+function sendEmail(contactForm){
+    emailjs.send("service_jdj2x9p", "rosie_zqn5tmi", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response){
+            console.log("SUCCESS", response);
+        },
+        function(error){
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
